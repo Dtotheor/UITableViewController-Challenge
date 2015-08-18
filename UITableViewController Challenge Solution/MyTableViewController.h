8dc8d938de254ce4c375d6ff4fadf5d6e1cd41ff
dpeled <dpeled@barneys.com>
2015-08-17 20:22:03 -0400

    tablieview lesson

---
+++ UITableViewController Challenge Solution/MyTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Doreen Peled on 8/12/15.
+//  Copyright (c) 2015 Doreen Peled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewController : UITableViewController
+
+@end
